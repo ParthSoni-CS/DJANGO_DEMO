aa7133bd2386f4cbba0650349c18b7a3d5e886de
ParthSoni <parthsoni0608@gmail.com>
2020-10-23 01:10:01 -0700

    This is my second Django commit, implemented dynamic name functionality with html in templates dict. .

--- web_dev/hello/views.py
+++ web_dev/hello/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Helloooo there")
+    return HttpResponse("Helloooo anonyms!")
+
+def greet(request,name):
+    return render(request, 'hello\index.html', {'name' : name})
+
