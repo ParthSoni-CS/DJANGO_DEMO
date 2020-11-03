198d6579e77444abf4b42072feb0c76329f7a3f4
ParthSoni <parthsoni0608@gmail.com>
2020-11-02 21:43:30 -0800

    This is my fifth django commit, just added form functionality using Django.forms lib and submit this form value to "todo" list and print it in todo tasks.

--- web_dev/tasks/urls.py
+++ web_dev/tasks/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path("",views.index, name = "home")
+    path("",views.index, name = "home"),
+    path("/add",views.add, name = "add")
 ]
--- web_dev/tasks/views.py
+++ web_dev/tasks/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django import forms
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority :", max_value=11, min_value=2)
 
 tasks = ["clean your desk","learn webdev","go out for a walk","learn to speak french"]
 
 def index(request):
     return render(request,"tasks/index.html",{'tasks':tasks})
+
+def add(request):
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            tasks.append(form.cleaned_data["task"])
+            return redirect("home")
+        else:
+            return render(request,"tasks/add.html",{'form':form})
+
+    return render(request,"tasks/add.html",{ 'form' : NewTaskForm()})
 
 # Create your views here.
