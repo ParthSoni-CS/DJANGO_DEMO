4b3a866a83af356d637d0d7c04991e0683113d37
ParthSoni <parthsoni0608@gmail.com>
2020-11-04 21:26:59 -0800

    This is my ninth commit, in this I've added dynamic page functionality to show the flight details, if id of flight is passed in URL then it shows the flight details of the flight related to the particular id

--- web_dev/flight/urls.py
+++ web_dev/flight/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path("",views.index,name="home")
+    path("",views.index,name="home"),
+    path("<int:flight_id>",views.flight_det,name="flight_det"),
 ]
 
--- web_dev/flight/views.py
+++ web_dev/flight/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from .models import flight
+
+# Create your views here.
 def index(request):
     return render(request, "flight/index.html", {
         "flights": flight.objects.all()
     })
-# Create your views here.
+
+def flight_det(request,flight_id):
+    flight_details = flight.objects.get(pk = flight_id)
+    return render(request, "flight/flight_det.html",{
+        "flight_det":flight_details
+    })
